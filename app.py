f7ea4289efd0ad0415d508c6edb91cac10f613a0
Goldie Yalamanchi <goldie.x.yalamanchi.-nd@disney.com>
2024-02-15 21:32:09 -0500

    me demo

--- app.py
+++ app.py
@@ -3,32 +3,22 @@ from customllm import CustomLLM
 from customembedder import CustomEmbedder
 
 from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import TextLoader
-from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain_community.vectorstores import Chroma
-from langchain.prompts import PromptTemplate
-from langchain.schema import StrOutputParser
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.schema.document import Document
 
-
-loader = TextLoader("./sotu2.txt")
-documents = loader.load()
-
-# split it into chunks
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=0)
-docs = text_splitter.split_documents(documents)
-
-# create the open-source embedding function
-#sentence_transformer_ef = SentenceTransformerEmbeddings(model_name="/opt/app/workspace/ai_projects/revature/all-MiniLM-L6-v2")
-sentence_transformer_ef = CustomEmbedder()
-
-# load it into Chroma
-#client = chromadb.HttpClient(host="localhost", port=8000)
-#chroma_client = chromadb.PersistentClient(path="/Users/goldieyalamanchi/workspace/chroma")
-#db = Chroma(client=client, collection_name="my_collection", embedding_function=sentence_transformer_ef)
-#db = Chroma.from_documents(docs, sentence_transformer_ef)
-db = Chroma.from_documents(docs, sentence_transformer_ef)
+template = "You are an assistant that helps users."
+system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+#human_template = """Context: {context}
+#Question: {question}"""
+human_template = """Question: {question}  Context: {context}"""
+human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
 
 question = "Who won the SuperBowl in 2018?"
 context = "The Philadelphia Eagles won the SuperBowl in 2018"
@@ -38,13 +28,42 @@ Answer: Let's think step by step."""
 llm = CustomLLM()
 #context = db.similarity_search("Tell me about Putin")
 #context = db.as_retriever().get_relevant_documents(context)
-print(context)
+#print(context)
 
 @cl.on_chat_start
-def main():    
-    prompt = PromptTemplate(template=template, input_variables=["context","question"])
+async def main():    
+    elements = [
+    cl.Image(name="image1", display="inline", path="./IMG_0624.jpg")
+    ]
+    await cl.Message(content="Hey there this is Goldie Happy Times AI Demo, dont mind the pumpkin!", elements=elements).send()
+    files = None
+
+    # Wait for the user to upload a file
+    while files == None:
+        files = await cl.AskFileMessage(
+            content="Please upload a text file to begin!",
+            accept=["text/plain"],
+            max_size_mb=20,
+            timeout=180,
+        ).send()
+
+    file = files[0]
+
+    msg = cl.Message(content=f"Processing `{file.name}`...")
+    await msg.send()
+
+    # Decode the file
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=0)
+    text = file.content.decode("utf-8")
+    print(text)
+    docs = [Document(page_content=x) for x in text_splitter.split_text(text)]
+    sentence_transformer_ef = CustomEmbedder()
+    db = Chroma.from_documents(docs, sentence_transformer_ef)
+
+    cl.user_session.set("db", db)
+    
     llm_chain = LLMChain(llm=llm,
-                          prompt=prompt,
+                          prompt=chat_prompt,
                           verbose=True)
     
     cl.user_session.set("llm_chain", llm_chain)
@@ -53,10 +72,10 @@ def main():
 async def main(message: cl.Message):
     # Retrieve the chain from the user session
     llm_chain = cl.user_session.get("llm_chain")  # type: LLMChain
-    context = db.similarity_search(message.content)
-    #context = db.as_retriever().get_relevant_documents(message.content)
-    #print(context)
+    db = cl.user_session.get("db")  # type: LLMChain
+    context = db.as_retriever().get_relevant_documents(message.content)
+    print(context)
     answer = llm_chain({"question": message.content, "context": context})
     #answer = StrOutputParser.parse(answer)
     #print(answer)
-    await cl.Message(answer).send()
+    await cl.Message(answer["text"]).send()
